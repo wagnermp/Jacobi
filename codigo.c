deba08c079a01c2fbe26eaf431ecf5ba0b596b83
Wagner Mariano <wmariano@id.uff.br>
2017-09-26 14:52:09 -0300

    leitura e impressão

--- codigo.c
+++ codigo.c
@@ -1,1 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+void imprime(double **M, int dim)
+{
+	int i, j;
+
+		for(i=0; i<dim; i++)
+		{
+		  for(j=0; j<dim+1; j++)
+		  {
+			printf("%.2lf \t", M[i][j]);
+		  }
+		printf("\n");
+		}
+	printf("\n");
+}
+	
+	
+void lermatriz(double **M, int dim, FILE*leitura)
+{
+	int i, j;
+	double a;
+
+		for(i=0; i<dim; i++)
+		{
+			M[i]=malloc((dim+1)*sizeof(double));
+		}
+	i=j=0;
+
+		while(fscanf(leitura, "%lf", &a) != EOF)
+		{
+			M[i][j]=a;
+			j++;
+			if(j==dim+1)
+				{
+					j=0;
+					i++;
+				}
+		}
+
+}
+
+int main(int argc, char *argv[])
+{
+	int i, j, dim;
+	double **M, a;
+	FILE *leitura;
+
+	leitura = fopen(argv[1],"r");
+	i = fscanf(leitura, "%d", &dim);
+	M = malloc(dim*sizeof(double*));
+
+	lermatriz(M,dim,leitura);
+
+	printf("matriz digitada: \n");
+	imprime(M,dim);
+
+
+
+}
